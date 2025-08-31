3378413671ab9221a1a0821f218442816dbaf367
Sanskar Gokhroo <sanskargokhroo1@gmail.com>
2025-08-31 20:55:25 +0000

    Error: An unexpected response was received from the server.     at fetch

--- next.config.ts
+++ next.config.ts
@@ -24,12 +24,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.externals.push('pdf-parse');
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
