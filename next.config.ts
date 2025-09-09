23ab83d4dd483e51503b735a46dead038a2448db
Sanskar Gokhroo <sanskargokhroo1@gmail.com>
2025-09-09 05:16:58 +0000

    aree firebase hosting paise maang rha hai isliye mujhe isse firebase par

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
+    serverComponentsExternalPackages: ["genkit"],
     serverActions: {
       bodySizeLimit: '10mb',
     },
